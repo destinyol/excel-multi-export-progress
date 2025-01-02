99649338618d3335d724792f50b38847377a1f9e
pyf <peng.yifu@grandlynn.com>
2025-01-02 16:17:24 +0800

    init

---
+++ src/main/java/com/example/excelmultiexportprogress/ExcelMultiExportProgressApplication.java
@@ -0,0 +1,13 @@
+package com.example.excelmultiexportprogress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelMultiExportProgressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelMultiExportProgressApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/excelmultiexportprogress/ExcelMultiExportProgressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.excelmultiexportprogress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelMultiExportProgressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
