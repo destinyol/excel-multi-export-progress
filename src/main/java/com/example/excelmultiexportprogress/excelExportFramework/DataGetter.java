c003fb685e134dfa6395c72fd2ea30c325bd41ab
pyf <peng.yifu@grandlynn.com>
2025-01-06 11:06:13 +0800

    init

--- src/main/java/com/example/excelmultiexportprogress/excelExportFramework/DataGetter.java
+++ src/main/java/com/example/excelmultiexportprogress/excelExportFramework/DataGetter.java
@@ -7,7 +7,7 @@ public interface DataGetter {
     /**
      * 分页读取数据（需实现）
      * @param pageSize 分页大小
-     * @param pageNum  页码
+     * @param pageNum  页码（从1开始： 1、2、3、4、5 ......）
      * @param sqlFilterClass  用户传入的用于筛选的类，没传则为null（每次回调该函数，该类都会初始化为一开始传入的样子）
      * @return
      */
